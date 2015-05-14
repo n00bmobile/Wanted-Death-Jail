a4ab937686e7954494dd3f1425a3e906e4b8537e
n00bmobile <matheus.moraes1277@gmail.com>
2015-05-14 17:29:02 -0300

    Uploaded

---
+++ lua/darkrp_modules/d_jailer/sv_jailer.lua
@@ -0,0 +1,10 @@
+#NoSimplerr#
+
+hook.Add("PlayerDeath", "checkForDeath", function(victim)
+    if victim:getDarkRPVar("wanted", true) then
+	    DarkRP.notify(victim, 1, 5, "You have been jailed since you were wanted and died!")
+	    victim:setDarkRPVar("wanted", false)
+		
+		RunConsoleCommand("rp_arrest", victim:Nick())
+	end
+end)
