f19f42dd9910e4b6ace4fa19b36ff3b8dc000951
n00bmobile <matheus.moraes1277@gmail.com>
2015-05-14 18:47:54 -0300

    Changed Something

--- lua/darkrp_modules/d_jailer/sv_jailer.lua
+++ lua/darkrp_modules/d_jailer/sv_jailer.lua
@@ -1,4 +1,4 @@
-hook.Add("PlayerDeath", "checkForDeath", function(victim)
+hook.Add("PlayerSpawn", "checkForDeath", function(victim)
     if victim:getDarkRPVar("wanted", true) then
 	    DarkRP.notify(victim, 1, 5, "You have been jailed since you were wanted and died!")
 	    victim:setDarkRPVar("wanted", false)
