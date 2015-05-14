f9ffae361e2adb231128374df52d1bbfb4a55803
n00bmobile <matheus.moraes1277@gmail.com>
2015-05-14 17:29:36 -0300

    Forgot To Remove #NoSimplerr#

--- lua/darkrp_modules/d_jailer/sv_jailer.lua
+++ lua/darkrp_modules/d_jailer/sv_jailer.lua
@@ -1,5 +1,3 @@
-#NoSimplerr#
-
 hook.Add("PlayerDeath", "checkForDeath", function(victim)
     if victim:getDarkRPVar("wanted", true) then
 	    DarkRP.notify(victim, 1, 5, "You have been jailed since you were wanted and died!")
