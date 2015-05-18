2920ddc5e2d0a55cc50db410a34bbdc3f29bbd7a
n00bmobile <matheus.moraes1277@gmail.com>
2015-05-17 22:59:33 -0300

    Forgot to change victim to ply :D

--- lua/darkrp_modules/d_jailer/sv_jailer.lua
+++ lua/darkrp_modules/d_jailer/sv_jailer.lua
@@ -1,8 +1,8 @@
-hook.Add("PlayerSpawn", "checkForDeath", function(victim)
-    if victim:getDarkRPVar("wanted", true) then
-	    DarkRP.notify(victim, 1, 5, "You have been jailed since you were wanted and died!")
-	    victim:setDarkRPVar("wanted", false)
+hook.Add("PlayerSpawn", "checkForDeath", function(ply)
+    if ply:getDarkRPVar("wanted", true) then
+	    DarkRP.notify(ply, 1, 5, "You have been jailed since you were wanted and died!")
+	    ply:setDarkRPVar("wanted", false)
 		
-		RunConsoleCommand("rp_arrest", victim:Nick())
+		RunConsoleCommand("rp_arrest", ply:Nick())
 	end
 end)
