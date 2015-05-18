18164e24f6dcbe40c4125b1457d738a447f86a65
n00bmobile <matheus.moraes1277@gmail.com>
2015-05-18 14:37:17 -0300

    Fixed MP issue

--- lua/darkrp_modules/d_jailer/sv_jailer.lua
+++ lua/darkrp_modules/d_jailer/sv_jailer.lua
@@ -1,8 +1,8 @@
 hook.Add("PlayerSpawn", "checkForDeath", function(ply)
     if ply:getDarkRPVar("wanted", true) then
-	    DarkRP.notify(ply, 1, 5, "You have been jailed since you were wanted and died!")
 	    ply:setDarkRPVar("wanted", false)
+		ply:arrest()
 		
-		RunConsoleCommand("rp_arrest", ply:Nick())
+		DarkRP.notify(ply, 1, 5, "You have been jailed since you were wanted and died!")
 	end
 end)
